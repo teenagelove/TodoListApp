95cb33cdd40c1afc479a7d8833563936607a0647
Danil Kazakov <dani4upa586@gmail.com>
2025-10-29 20:33:30 +0900

    chore: initialize project with .gitignore and README

---
+++ TodoListApp/TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Danil Kazakov on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
