0053138b7766ad556d79e9dc60ee61ec83d719e5
Danil Kazakov <dani4upa586@gmail.com>
2025-11-03 16:12:03 +0900

    refactor(Entity): update TodoTask Codable conformance
    changes TodoTask conformance from 'Codable' to 'Decodable' as it's only used for decoding from external sources. this refines the entity's contract to reflect its actual usage.

--- TodoListApp/Modules/TodoDetail/Entity/TodoTask.swift
+++ TodoListApp/Modules/TodoDetail/Entity/TodoTask.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct TodoTask: Identifiable, Hashable, Codable {
+struct TodoTask: Identifiable, Hashable, Decodable {
     let id: UUID
     let title: String
     let description: String
